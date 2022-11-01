c242451529ddf208cbe8364c8edbaa14b02eb5e6
Ondrej Mirtes <ondrej@mirtes.cz>
2022-11-01 14:44:27 +0100

    Revert "Call TypeSpecifier for native types too"
    This reverts commit ffecded9e4b9a4b6bc5740633df2ee63394293bf.

--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -3625,11 +3625,6 @@ class MutatingScope implements Scope
 
 		$specifiedTypes = $this->typeSpecifier->specifyTypesInCondition($this, $expr, TypeSpecifierContext::createTruthy());
 		$scope = $this->filterBySpecifiedTypes($specifiedTypes);
-
-		$nativeScope = $this->promoteNativeTypes();
-		$nativeSpecifiedTypes = $this->typeSpecifier->specifyTypesInCondition($nativeScope, $expr, TypeSpecifierContext::createTruthy());
-		$scope->nativeExpressionTypes = $nativeScope->filterBySpecifiedTypes($nativeSpecifiedTypes)->expressionTypes;
-
 		$this->truthyScopes[$exprString] = $scope;
 
 		return $scope;
@@ -3648,10 +3643,6 @@ class MutatingScope implements Scope
 
 		$specifiedTypes = $this->typeSpecifier->specifyTypesInCondition($this, $expr, TypeSpecifierContext::createFalsey());
 		$scope = $this->filterBySpecifiedTypes($specifiedTypes);
-		$nativeScope = $this->promoteNativeTypes();
-		$nativeSpecifiedTypes = $this->typeSpecifier->specifyTypesInCondition($nativeScope, $expr, TypeSpecifierContext::createFalsey());
-		$scope->nativeExpressionTypes = $nativeScope->filterBySpecifiedTypes($nativeSpecifiedTypes)->expressionTypes;
-
 		$this->falseyScopes[$exprString] = $scope;
 
 		return $scope;
